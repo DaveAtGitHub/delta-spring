98c39c42ae1af059db33731d801f532c021cec09
Wanner <dwanner@Cardinals-MacBook-Pro.local>
2017-08-11 11:32:54 -0400

    These are services for the Delta project, that being moving from Angular1 to Angular2.

---
+++ src/main/java/com/example/demo/DeltaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaSpringApplication.class, args);
+	}
+}
